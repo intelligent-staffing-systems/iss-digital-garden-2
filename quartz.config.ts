69aa353aeb494a4ffa1493b72d583120d8579bda
ldraney <draneylucas@gmail.com>
2024-09-21 16:41:10 -0600

    ISS

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Intelligent Staffing Systems",
+    pageTitle: "ISS",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
